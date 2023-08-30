35081a15a21d490b37cbd258c9da7b057ce7ca39
IstvanZapfel <105928213+IstvanZapfel@users.noreply.github.com>
2023-08-30 21:08:26 +0200

    Delete gulp directory

--- gulp/config/ftp.js
+++
@@ -1,6 +0,0 @@
-export let configFTP = {
-	host: "files.000webhost.com", // ftp adress
-	user: "testsiteonly2023", // user name
-	password: "Testsite444", //password
-	parallel: 5 // number of simultaneous streams
-}
